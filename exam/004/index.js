03772d2c97eb5f1c4f67b4d62b6ef161f7a4e082
shenjinxiang <sjx-sword@163.com>
2020-07-02 10:43:35 +0800

    vue-example if and for

---
+++ exam/004/index.js
@@ -0,0 +1,12 @@
+let app = new Vue({
+	el: '#app',
+	data: {
+		list: [
+			{xh: 1, text: 'java'},
+			{xh: 2, text: 'python'},
+			{xh: 3, text: 'javascript'},
+			{xh: 4, text: 'C++'},
+			{xh: 5, text: 'Go'},
+		]
+	}
+});
