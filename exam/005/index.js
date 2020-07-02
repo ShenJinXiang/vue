36d963cd7348a19af779503649667dd4d961d368
shenjinxiang <sjx-sword@163.com>
2020-07-02 16:29:50 +0800

    vue bind / model example

---
+++ exam/005/index.js
@@ -0,0 +1,11 @@
+let app = new Vue({
+	el: '#app',
+	data: {
+		message: 'Hello Vue!',
+	},
+	methods: {
+		reverseMessage: function() {
+			this.message = this.message.split('').reverse().join('');
+		}
+	}
+});
