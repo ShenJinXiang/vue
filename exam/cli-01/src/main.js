b02949e047c4f82e358b26cbc31261501a5fe6bb
shenjinxiang <sjx-sword@163.com>
2021-06-09 07:44:15 +0800

    element table-data1

--- exam/cli-01/src/main.js
+++ exam/cli-01/src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue';
 // import axios from 'axios';
 // import VueAxios from 'vue-axios';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue';
 import router from './router';
 
 Vue.config.productionTip = false;
 // Vue.use(VueAxios, axios);
+Vue.use(ElementUI);
 
 new Vue({
   router,
--- exam/cli-01/src/views/element/demo1.vue
+++ exam/cli-01/src/views/element/demo1.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="element-con">
     <h4>{{ title }}</h4>
+    <table-data1></table-data1>
   </div>
 </template>
 <script>
+import TableData1 from '@/components/element/TableData1.vue';
+
 export default {
   name: 'elementDemo1',
   data() {
     return {
       title: 'element-exam-demo1',
     };
+  },
+  components: {
+    TableData1,
   },
 };
 </script>
